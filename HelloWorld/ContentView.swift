e5a09e51a98baf644ede061b5f5dda60fbda32c5
shin.ota <shin.ota0817@gmail.com>
2020-10-14 11:52:40 +0900

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Shin Ota on 10/14/20.
+//  Copyright © 2020 Shin Ota. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
